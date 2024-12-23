d9591d97a87da27cae356c804cda2ac59e62daa1
Sniper0141 <116571645+Sniper0141@users.noreply.github.com>
2024-12-23 16:40:46 +0100

    mouse pointer aim works

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -5,16 +5,29 @@ namespace Player
 {
     public class PlayerInput : MonoBehaviour
     {
-        private InputAction mouseMoveAction;
+        public float sensitivity;
         
-        void Start()
+        private InputAction mouseMoveAction;
+        private Vector3 currentTargetVector;
+
+        private void Start()
         {
             mouseMoveAction = InputSystem.actions.FindAction("Look");
+            //mouseMoveAction.Enable();
         }
 
-        void Update()
+        private void Update()
         {
+            var mouseMoveVector = mouseMoveAction.ReadValue<Vector2>();
             
+            var newTargetPosition = transform.position;
+            newTargetPosition.x += mouseMoveVector.x * sensitivity;
+            newTargetPosition.y += mouseMoveVector.y * sensitivity;
+            
+            newTargetPosition.x = Mathf.Clamp(newTargetPosition.x, -5f, 5f);
+            newTargetPosition.y = Mathf.Clamp(newTargetPosition.y, -3f, 3f);
+            
+            transform.position = newTargetPosition;
         }
     }
 }
