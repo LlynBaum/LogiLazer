ca2a323dd9a8e098843b49d9b3959dd64c99dfff
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-12-23 15:47:25 +0100

    parcels

---
+++ Assets/Scripts/Parcels/Parcel.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Parcels
+{
+    public class Parcel : MonoBehaviour
+    {
+        public float minSpeed;
+        public float maxSpeed;
+
+        public float minHeight;
+        public float maxHeight;
+        
+        private Vector3 spawnPosition;
+        private Transform target;
+
+        private float height;
+        private float speed;
+        private float time;
+
+        private void Start()
+        {
+            spawnPosition = transform.position;
+            target = ParcelManager.Instance.target;
+            
+            speed = Random.Range(minSpeed, maxSpeed);
+            height = Random.Range(minHeight, maxHeight);
+            time = 0f;
+        }
+
+        private void Update()
+        {
+            if (time < 1f)
+            {
+                time += Time.deltaTime * speed;
+                Vector3 newPosition = CalculateParabolicPoint(time, spawnPosition, target.position, height);
+                transform.position = newPosition;
+            }
+        }
+
+        private static Vector3 CalculateParabolicPoint(float t, Vector3 start, Vector3 end, float h)
+        {
+            var horizontal = Vector3.Lerp(start, end, t);
+            var vertical = h * (1 - Mathf.Pow(2 * t - 1, 2));
+            return new Vector3(horizontal.x, horizontal.y + vertical, horizontal.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Parcels/ParcelManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Parcels
+{
+    public class ParcelManager : MonoBehaviour
+    {
+        public static ParcelManager Instance;
+        
+        public GameObject prefab;
+        public Transform target;
+        public int rate;
+
+        public float maxOffSetX;
+        public float maxOffSetY;
+        
+        private void Start()
+        {
+            Instance = this;
+            transform.LookAt(target);
+            InvokeRepeating(nameof(LaunchParcel), 3, rate);
+        }
+
+        private void LaunchParcel()
+        {
+            var xOffSet = Random.Range(-maxOffSetX, maxOffSetX);
+            var yOffSet = Random.Range(-maxOffSetY, maxOffSetY);
+            var position = new Vector3(xOffSet, yOffSet) + transform.position;
+            Debug.Log(position);
+            Instantiate(prefab, position, transform.rotation);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawCube(transform.position, new Vector3(maxOffSetX, maxOffSetY) * 2);
+        }
+    }
+}
