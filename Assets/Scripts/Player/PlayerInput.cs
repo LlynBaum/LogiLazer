cbe82e55e83871a9fc8bc841f57426d85f79eb07
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-12-24 11:31:44 +0100

    shoot

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -6,27 +6,49 @@ namespace Player
     public class PlayerInput : MonoBehaviour
     {
         public float sensitivity;
-        
-        private InputAction mouseMoveAction;
-        private Vector3 currentTargetVector;
+        public Transform raycast;
+        public float raycastLength;
+        public LayerMask layerMask;
+
+        private int parcelMask;
 
         private void Start()
         {
-            mouseMoveAction = InputSystem.actions.FindAction("Look");
-            //mouseMoveAction.Enable();
+            parcelMask = 1 << layerMask;
         }
 
-        private void Update()
+        public void OnShoot(InputAction.CallbackContext context)
         {
-            var mouseMoveVector = mouseMoveAction.ReadValue<Vector2>();
+            if (Raycast(out var hit))
+            {
+                GameManager.Instance.ParcelHit(hit.collider.gameObject);
+            }
+        }
+
+        private bool Raycast(out RaycastHit hit)
+        {
+            Gizmos.color = Color.magenta;
+            Debug.DrawRay(raycast.position, raycast.TransformDirection(transform.forward) * 50, Color.magenta, 1f);
             
+            return Physics.Raycast(
+                origin: raycast.position, 
+                direction: raycast.TransformDirection(raycast.forward),
+                hitInfo: out hit,
+                maxDistance: raycastLength, 
+                layerMask: parcelMask);
+        }
+
+        public void OnMouseMovement(InputAction.CallbackContext context)
+        {
+            var mouseMoveVector = context.ReadValue<Vector2>();
+
             var newTargetPosition = transform.position;
             newTargetPosition.x += mouseMoveVector.x * sensitivity;
             newTargetPosition.y += mouseMoveVector.y * sensitivity;
-            
+
             newTargetPosition.x = Mathf.Clamp(newTargetPosition.x, -5f, 5f);
-            newTargetPosition.y = Mathf.Clamp(newTargetPosition.y, -3f, 3f);
-            
+            newTargetPosition.y = Mathf.Clamp(newTargetPosition.y, -5f, 5f);
+
             transform.position = newTargetPosition;
         }
     }
