fc38d78cb8f128da224a27ed581ec5de93bf8102
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-01-03 11:52:53 +0100

    better aim

--- Assets/Scripts/ProGlove/ProGloveIk.cs
+++ Assets/Scripts/ProGlove/ProGloveIk.cs
@@ -4,13 +4,21 @@ namespace ProGlove
 {
     public class ProGloveIk : MonoBehaviour
     {
-        public Transform targetTransform;
+        public Camera mainCamera;
         public Transform bone;
         
         private void LateUpdate()
         {
-            var targetPosition = targetTransform.position;
-            AimAt(targetPosition);
+            var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit))
+            {
+                AimAt(hit.point);
+            }
+            else
+            {
+                var targetPosition = ray.origin + ray.direction * 50f;
+                AimAt(targetPosition);
+            }
         }
 
         private void AimAt(Vector3 targetPosition)
@@ -19,6 +27,7 @@ namespace ProGlove
             var targetDirection = (targetPosition - transform.position).normalized;
             
             var aimTowards = Quaternion.FromToRotation(currentDirection, targetDirection);
+            
             bone.rotation = aimTowards * bone.rotation;
         }
     }
